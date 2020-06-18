da12fd6a983e14162eefcb0201056feeadfa3dd6
hyun <mintcherry@users.noreply.github.com>
2020-06-18 14:58:14 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define ll long long
 #define pi pair<int, int>
 #define vi vector<int>
+#define vpi vector<pair<int, int> >
 #define si stack<int>
 #define pb push_back
 #define mp make_pair
@@ -14,6 +15,8 @@ using namespace std;
 #define FOREACH(i, CONT) for (VAR(i, (CONT).begin()); i != (CONT).end(); ++i)
 #define FOREACHD(i, CONT) for (VAR(i, (CONT).rbegin()); i != (CONT).rend(); ++i)
 #define REP(i, n) FOR(i, 0, n)
+#define CLR(a, x) memset(a, x, sizeof(a))
+#define SIZE(x) ((int)x.size())
 #define ld long double
 constexpr int INF = 1000000009;
 constexpr long long INFLL = (ll)INF * (ll)INF;
@@ -25,18 +28,18 @@ void read(T& a) {cin >> a;}
 template<typename T>
 void read(vector<T>& v, int n) {int tmp; REP(i, n) {read(tmp); v.pb(tmp);};}
 template<typename T, typename V>
-void read(pair<T, V>& p) {read(p.x); read(p.y);}
+void read(pair<T, V>& p) {read(p.first); read(p.second);}
 template<typename T>
-void print(const T& a) {cout << a << "\n";}
-void print(const char *s) {printf("%s\n", s);}
+void println(const T& a) {cout << a << "\n";}
+void println(const char *s) {printf("%s\n", s);}
 template<typename T>
-void print_(const T& a) {cout << a << " ";}
+void print(const T& a) {cout << a << " ";}
 template<typename T>
-void print(const vector<T>& v) {for (const auto &i : v) print_(i); cout << "\n";}
+void println(const vector<T>& v) {for (const auto &i : v) print(i); cout << "\n";}
+template<typename T>
+void print(const vector<T>& v) {for (const auto &i : v) print(i); cout << " ";}
 template<typename T, typename V>
-void print_(const vector<T>& v) {for (const auto &i : v) print_(i); cout << " ";}
+void println(const pair<T, V>& p) {print(p.first); println(p.second);}
 template<typename T, typename V>
-void print(const pair<T, V>& p) {print_(p.x); print(p.y);}
-template<typename T, typename V>
-void print_(const pair<T, V>& p) {print_(p.x); print_(p.y);}
+void print(const pair<T, V>& p) {print(p.first); print(p.second);}
 /////////////////////////////////////////////////////////////////////////
